1aea62badd2d3a31e91f18df6f9fda27d8633bd5
Boris <boris@email.com>
2021-09-11 09:21:11 +1000

    third commit

--- index.js
+++ index.js
@@ -6,24 +6,48 @@ const fetchData = async (strSearch) => {
             s: strSearch
         }
     });
+
+    if(response.data.Error){
+        return [];
+    }
+
+
     return response.data.Search;
-}
+};
+
+const root = document.querySelector('.autofinish');
+
+root.innerHTML =  `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+    </div>
+    
+`;
 
 //listen for input
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
 const onInput = async event =>{
     const movies = await fetchData(event.target.value);
-    console.log(movies);
-
+    
+    resultsWrapper.innerHTML='';
+    dropdown.classList.add('is-active');
     for ( let movie of movies ){
-        const div = document.createElement('div');
+        const option = document.createElement('a');
 
-        div.innerHTML = `
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
         <img src="${movie.Poster}" />
-        <h2>${movie.Title}</h2>
+        ${movie.Title}
         `;
 
-        document.querySelector('#target').appendChild(div);
+        resultsWrapper.appendChild(option);
     }
 
 
