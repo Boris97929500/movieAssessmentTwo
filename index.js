bd3a7772630e9bae701e84e75425e225430905e4
Boris <boris@email.com>
2021-09-12 06:40:42 +1000

    adding more functionality

--- index.js
+++ index.js
@@ -1,111 +1,25 @@
-//fetch data
-const fetchData = async (strSearch) => {
-    const response = await axios.get('http://www.omdbapi.com', {
-        params: {
-            apikey: '2770e2df',
-            s: strSearch
-        }
-    });
+const express = require('express')
+const app = express()
+const allData = [];
+app.use(express.static('public'))
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-    if(response.data.Error){
-        return [];
-    }
+app.listen(3000, ()=>{
+    console.log('listening on port 3000')
+} )
 
-
-    return response.data.Search;
-};
-
-const root = document.querySelector('.autofinish');
-
-root.innerHTML =  `
-    <label><b>Search For a Movie</b></label>
-    <input class="input" />
-    <div class="dropdown">
-    <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-    </div>
-    </div>
-    
-`;
-
-//listen for input
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async event =>{
-
-
-    const movies = await fetchData(event.target.value);
-
-    if( movies == ''){
-        dropdown.classList.remove('is-active');  
-        return;      
-    }
-
-    resultsWrapper.innerHTML='';
-    dropdown.classList.add('is-active');
-    for ( let movie of movies ){
-        const option = document.createElement('a');
-        const imgSrc = movie.Poster === 'N/A' ? '': movie.Poster;
-
-        option.classList.add('dropdown-item');
-        option.innerHTML = `
-        <img src="${imgSrc}" />
-        ${movie.Title}
-        `;
-
-        option.addEventListener('click',()=>{
-            dropdown.classList.remove('is-active');
-            input.value=movie.Title;
-            movieClicked(movie);
-        })
-
-        resultsWrapper.appendChild(option);
-    }
-
-
-
-}
-//debounce function from loaddash
-input.addEventListener('input', 
-    _.debounce(onInput, 1000)
-)
-
-document.addEventListener('click', event =>{
-    if(!root.contains(event.target)){
-        dropdown.classList.remove('is-active');
-    }
+app.post('/api', (req, res) =>{
+    const data = req.body
+    allData.push(data)
+    console.log(allData)
+    res.json(allData)
 })
 
-const movieClicked = async movie =>{
-    const response = await axios.get('http://www.omdbapi.com', {
-        params:{
-            apikey: '2770e2df',
-            i: movie.imdbID
-        }
-    })
+app.get('/api', (req,res)=>{
+    res.json(allData)
+})
 
-    document.querySelector('#movieStats').innerHTML = showDetails(response.data);
-}
-
-const showDetails = (filmInfo) =>{
-    return `
-    <article class="media">
-      <figure class="media-left">
-        <p class="image">
-          <img src="${filmInfo.Poster}" />
-        </p>
-      </figure>
-      <div class="media-content">
-        <div class="content">
-          <h1>${filmInfo.Title}</h1>
-          <h4>${filmInfo.Genre}</h4>
-          <p>${filmInfo.Plot}</p>
-        </div>
-      </div>
-    </article>
-
-
-  `
-}
+// app.get('/api', (req, res)=>{
+//     res.render('ejsFile/index.ejs',{allData})
+// })
---
+++ public/indexfrontend.js
@@ -0,0 +1,135 @@
+let movieTitle = "";
+
+//fetch data
+const fetchData = async (strSearch) => {
+    const response = await axios.get('http://www.omdbapi.com', {
+        params: {
+            apikey: '2770e2df',
+            s: strSearch
+        }
+    });
+
+    if(response.data.Error){
+        return [];
+    }
+
+
+    return response.data.Search;
+};
+
+const root = document.querySelector('.autofinish');
+
+root.innerHTML =  `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+    </div>
+    
+`;
+
+//listen for input
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+const onInput = async event =>{
+
+
+    const movies = await fetchData(event.target.value);
+
+    if( movies == ''){
+        dropdown.classList.remove('is-active');  
+        return;      
+    }
+
+    resultsWrapper.innerHTML='';
+    dropdown.classList.add('is-active');
+    for ( let movie of movies ){
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '': movie.Poster;
+
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
+        `;
+
+        option.addEventListener('click',()=>{
+            dropdown.classList.remove('is-active');
+            input.value=movie.Title;
+            movieClicked(movie);
+        })
+
+        resultsWrapper.appendChild(option);
+    }
+
+
+
+}
+//debounce function from loaddash
+input.addEventListener('input', 
+    _.debounce(onInput, 1000)
+)
+
+document.addEventListener('click', event =>{
+    if(!root.contains(event.target)){
+        dropdown.classList.remove('is-active');
+    }
+})
+
+const movieClicked = async movie =>{
+    const response = await axios.get('http://www.omdbapi.com', {
+        params:{
+            apikey: '2770e2df',
+            i: movie.imdbID
+        }
+    })
+    //console log out movie title
+    console.log(response.data.Title)
+    movieTitle = response.data.Title
+    document.querySelector('#movieStats').innerHTML = showDetails(response.data);
+}
+
+const showDetails = (filmInfo) =>{
+    return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${filmInfo.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${filmInfo.Title}</h1>
+          <h4>${filmInfo.Genre}</h4>
+          <p>${filmInfo.Plot}</p>
+        </div>
+      </div>
+    </article>
+
+
+  `
+}
+
+document.getElementById('submitButton').addEventListener('click', async (event)=>{
+    const data = movieTitle ;
+    const demoData = 'hello world';
+
+    // const options = 
+    // {
+    //     method: 'POST',
+    //     headers: {'Content-Type':'application/json'},
+    //     body: JSON.stringify(demoData)
+    // }
+    const response = await axios({
+        method: 'post',
+        url: 'http://localhost:3000/api',
+        data: {
+            data
+        }
+    })
+
+})
